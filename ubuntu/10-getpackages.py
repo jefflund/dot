140e19c1b2ce616df8a257aa824fa49e595b4099
Jeffrey Lund <jefflund@gmail.com>
2013-07-02 19:12:15 -0700

    get-packages works from anywhere

--- ubuntu/10-getpackages.py
+++ ubuntu/10-getpackages.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import os
+from os import path
 
-lines = open('install').readlines()
+lines = open(path.join(path.dirname(__file__), 'install')).readlines()
 lines = [line.strip() for line in lines]
 lines = [line for line in lines if line]
 cmd = ['apt-get', 'install', '-y'] + lines
