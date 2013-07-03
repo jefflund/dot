484491e28ac3f3fb6115984a5f13af742bcee8e4
Jeffrey Lund <jefflund@gmail.com>
2013-07-02 20:26:11 -0700

    Added dirs to 10-dirs.py

--- homedir/10-dirs.py
+++ homedir/10-dirs.py
@@ -6,13 +6,23 @@ import shutil
 home = os.environ['HOME']
 rm_dirs = ['Documents',
            'Downloads',
-	   'Desktop',
-	   'Music',
-	   'Pictures',
+           'Desktop',
+           'Music',
+           'Pictures',
            'Public',
            'Templates',
            'Videos']
+mk_dirs = ['library',
+              'desktop',
+              'go']
 
 for rm_dir in rm_dirs:
 	print 'Removing', rm_dir
 	shutil.rmtree(os.path.join(home, rm_dir), True)
+
+for mk_dir in mk_dirs:
+    print 'Adding', mk_dir
+    try:
+        os.makedirs(os.path.join(home, mk_dir))
+    except OSError:
+        pass
