1e0e9e3ff43ddc5715a412bdb5eb3a5d02c69d9c
Jeffrey Lund <jefflund@gmail.com>
2014-02-08 20:20:12 -0700

    Changed ycm.sh to be symlink to install script

--- vim/30-ycm.sh
+++ vim/30-ycm.sh
@@ -1,16 +1,1 @@
-#!/usr/bin/env bash
-
-if [ -e "$HOME/.vim/bundle/YouCompleteMe/python/ycm_core.so" ]
-then
-    exit
-fi
-
-cd $HOME
-mkdir ycm_build
-cd ycm_build
-
-cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/cpp
-make ycm_core
-
-cd $HOME
-rm -rf ycm_build
+/home/jlund3/.vim/bundle/YouCompleteMe/install.sh
