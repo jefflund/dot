31677ae8df66e3582d2afd09cdaec1966a8da10d
Jeffrey Lund <jefflund@gmail.com>
2015-05-01 15:04:28 -0700

    Prevent glt *.so from being run as part of install

--- install/30-glt.py
+++ install/30-glt.py
@@ -6,7 +6,7 @@ import shutil
 
 srcdir = os.path.dirname(__file__)
 
-shutil.copy(os.path.join(srcdir, 'BearLibTerminal.h'), '/usr/include/BearLibTerminal.h')
-shutil.copy(os.path.join(srcdir, 'libBearLibTerminal.so.32'), '/usr/lib/libBearLibTerminal.so')
+shutil.copy(os.path.join(srcdir, 'lib/BearLibTerminal.h'), '/usr/include/BearLibTerminal.h')
+shutil.copy(os.path.join(srcdir, 'lib/libBearLibTerminal.so.32'), '/usr/lib/libBearLibTerminal.so')
 if platform.machine() == 'x86_64':
-    shutil.copy(os.path.join(srcdir, 'libBearLibTerminal.so.64'), '/usr/lib64/libBearLibTerminal.so')
+    shutil.copy(os.path.join(srcdir, 'lib/libBearLibTerminal.so.64'), '/usr/lib64/libBearLibTerminal.so')
