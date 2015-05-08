d27e14b3795727eb93d56b6710b6af34e4360a64
Jeffrey Lund <jefflund@gmail.com>
2015-05-08 11:41:54 -0700

    60-data uses common

--- homedir/60-data.py
+++ homedir/60-data.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python
 
 import os
-from os import path
+import imp
 import socket
 
-def link_exists(src, dst):
-    return (path.exists(dst) and
-            path.islink(dst) and
-            path.samefile(src, dst))
-
 home = os.environ['HOME']
-links = [('/aml/scratch/jlund3/data', 'data'),
-         ('/aml/scratch/jlund3', 'scratch'),
-         ('/aml/git/jlund3', 'git'),
-         ('/local/jlund3', 'local')]
+lib = imp.load_source('lib', os.path.join(home, 'config', 'bin', 'common.py'))
 
 if 'aml.cs.byu.edu' in socket.gethostname():
-    for target, name in links:
-        name = path.join(home, name)
-        if not link_exists(target, name):
-            print 'Linking', path.relpath(name)
-            os.symlink(target, name)
+    links = [('/aml/scratch/jlund3/data', 'data'),
+             ('/aml/scratch/jlund3', 'scratch'),
+             ('/aml/git/jlund3', 'git'),
+             ('/local/jlund3', 'local')]
+    for src, dst in links:
+        lib.ensure_link(src, os.path.join(home, dst))
 else:
-    # TODO remove code duplication between this and 20-clone
-    path = os.path.join(os.environ['HOME'], 'research/data')
-    if not os.path.exists(path):
-        try:
-            os.makedirs(path)
-            os.system('git clone git@github.com:jlund3/data',
-                       os.path.join(os.environ['HOME'], 'research/data'))
-        except OSError:
-            pass
+    lib.clone('git@github.com:jlund3/data', 'research/data')
