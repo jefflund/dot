891d9c9a40929ac12adc8d4357a9b75976a50f35
Jeffrey Lund <jefflund@gmail.com>
2015-12-30 12:07:00 -0700

    Converted bin scripts to python3

--- bin/common.py
+++ bin/common.py
@@ -1,8 +1,9 @@
+"""Common utils for config python scripts"""
+
 import os
 
-home = os.environ['HOME']
-
 def ensure_filedir(filename):
+    """Ensures that the directory path of a file exists"""
     try:
         os.makedirs(os.path.dirname(filename))
     except OSError:
@@ -10,6 +11,7 @@ def ensure_filedir(filename):
 
 
 def ensure_removed(path):
+    """Ensures that a file path is removed"""
     try:
         if os.path.isdir(path):
             os.rmdir(path)
@@ -20,21 +22,24 @@ def ensure_removed(path):
 
 
 def link_exists(src, dst):
+    """Returns true if the dst is a symlink to the src"""
     return (os.path.exists(dst) and
             os.path.islink(dst) and
             os.path.samefile(src, dst))
 
 
 def ensure_link(src, dst):
+    """Ensures that the dst is a symlink to the src"""
     if not link_exists(src, dst):
-        print 'Linking', dst
+        print('Linking', dst)
         ensure_filedir(dst)
         ensure_removed(dst)
         os.symlink(src, dst)
 
 
 def clone(repo, path):
-    path = os.path.join(home, path)
+    """Clones a git repo to a give path relative to the home directory"""
+    path = os.path.join(os.environ['HOME'], path)
     if not os.path.exists(path):
         ensure_filedir(path)
         os.system('git clone {} {}'.format(repo, path))
