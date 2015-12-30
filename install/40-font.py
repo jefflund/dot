accf1d42bc211f61d2a600869bc9d6859f977880
Jeffrey Lund <jefflund@gmail.com>
2015-12-30 12:21:32 -0700

    Removed glt install

--- install/40-font.py
+++ install/40-font.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+"""Adds the hack font"""
 
 import os
 import glob
 import shutil
 
-srcdir = os.path.dirname(__file__)
+def main():
+    """Adds the hack font"""
+    srcdir = os.path.dirname(__file__)
 
-for src in glob.glob(os.path.join(srcdir, 'hack', '*.ttf')):
-    dst = os.path.join('/usr/share/fonts', os.path.split(src)[1])
-    shutil.copy(src, dst)
+    for src in glob.glob(os.path.join(srcdir, 'hack', '*.ttf')):
+        dst = os.path.join('/usr/share/fonts', os.path.split(src)[1])
+        shutil.copy(src, dst)
+
+if __name__ == '__main__':
+    main()
