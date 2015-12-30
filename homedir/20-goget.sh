b96914ef91303ad74b882b188ed254f09dc26ffe
Jeffrey Lund <jefflund@gmail.com>
2015-12-30 12:23:10 -0700

    Removed glt get

--- homedir/20-goget.sh
+++ homedir/20-goget.sh
@@ -4,4 +4,3 @@ export GOPATH=$HOME/go
 go get -u github.com/nsf/termbox-go
 go get -u github.com/skelterjohn/go.matrix
 go get -u github.com/dchest/stemmer/porter2
-go get -u github.com/mpatraw/gopherlibterminal
