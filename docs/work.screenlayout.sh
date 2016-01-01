3506acd24ec0b6e043c5caceea5581cd477e27d7
Jeffrey Lund <jefflund@gmail.com>
2016-01-01 06:22:19 -0700

    Removed duplicate xrandr script

--- docs/work.screenlayout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP1 --mode 2560x1600 --pos 0x0 --rotate normal --output HDMI3 --off --output HDMI2 --mode 1920x1200 --pos 2560x0 --rotate left --output HDMI1 --off --output VGA1 --off
