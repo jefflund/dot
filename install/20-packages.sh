4a901f11f03de2ec96da9a5d4c0c589f0aa41d92
Jeffrey Lund <jefflund@gmail.com>
2016-02-09 17:36:29 -0700

    Smaller package list

--- install/20-packages.sh
+++ install/20-packages.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-for filename in $(find $HOME/config/install/packages/*); do
- 	sudo apt-get install -y $(grep -vE "^\s*#" $filename  | tr "\n" " ")
-done
+sudo apt-get install -y $(grep -vE "^\s*#" $HOME/config/install/packages  | tr "\n" " ")
