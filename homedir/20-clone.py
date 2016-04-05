84ec07651b1e112beff80ec6c29b4ec02af7c2e2
Jeffrey Lund <jefflund@gmail.com>
2016-04-05 11:30:39 -0600

    Moved rauko stuff to gitlab

--- homedir/20-clone.py
+++ homedir/20-clone.py
@@ -9,19 +9,21 @@ REPOS = {'git@github.com:jlund3/ankura': 'research/ankura',
          'git@bitbucket.org:jlund3/research': 'research/writing',
          'git@github.com:ezubaric/pinafore-papers': 'research/pinafore',
 
-         'git@gh-rauko:rauko1753/gorl': 'go/src/github.com/rauko1753/gorl',
-         'git@gh-rauko:rauko1753/stones': 'go/src/github.com/rauko1753/stones',
-         'git@gh-rauko:rauko1753/rauko1753.github.io': 'hobby/blog',
-         'git@gh-rauko:rauko1753/goomba': 'go/src/github.com/rauko1753/goomba',
-         'git@gh-rauko:rauko1753/foobot': 'hobby/foobot',
+         'git@gitlab.com:rauko1753/gorl': 'go/src/gitlab.com/rauko1753/gorl',
+         'git@gitlab.com:rauko1753/stones': 'go/src/gitlab.com/rauko1753/stones',
+         'git@gitlab.com:rauko1753/rauko1753.github.io': 'hobby/blog',
+         'git@gitlab.com:rauko1753/goomba': 'go/src/gitlab.com/rauko1753/goomba',
+         'git@gitlab.com:rauko1753/foobot': 'hobby/foobot',
 
          'git@github.com:jlund3/goplay': 'tools/goplay',
          'git@github.com:jlund3/git-horror': 'tools/git-horror',
          'git@github.com:byu-aml-lab/window': 'tools/window'}
 
-LINKS = {'go/src/github.com/rauko1753/gorl': 'hobby/gorl',
-         'go/src/github.com/rauko1753/stones': 'hobby/stones',
-         'go/src/github.com/rauko1753/goomba': 'hobby/goomba'}
+LINKS = {'go/src/gitlab.com/rauko1753/gorl': 'hobby/gorl',
+         'go/src/gitlab.com/rauko1753/stones': 'hobby/stones',
+         'go/src/gitlab.com/rauko1753/goomba': 'hobby/goomba'}
+
+# TODO Add git rauko command for hobby projects
 
 
 def main():
@@ -30,8 +32,8 @@ def main():
     lib_path = os.path.join(home, 'config', 'bin', 'common.py')
     lib = imp.load_source('lib', lib_path)
 
-    for repo, path in REPOS.items():
-        lib.clone(repo, path)
+    # for repo, path in REPOS.items():
+        # lib.clone(repo, path)
     for src, dst in LINKS.items():
         lib.ensure_link(os.path.join(home, src), os.path.join(home, dst))
 
