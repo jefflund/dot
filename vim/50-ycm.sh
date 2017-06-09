1b3834a8f08718883c1d3c99c0d7ba079e636054
Jeffrey Lund <jefflund@gmail.com>
2017-06-09 11:49:11 -0600

    Ready for merge (I hope)

--- vim/50-ycm.sh
+++ vim/50-ycm.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# note: On t1.micro need to create a swap file first
+# note: On systems with little ram (e.g., f1-micro) need to create a swap file
 # see ~/config/docs/swapfile for info on how to do this
-
 
 cd ~/.vim/bundle/YouCompleteMe
 python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --gocode-completer
