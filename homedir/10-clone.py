4c033868d6ca1d2a5bd16a6dc75859fbe7c72908
Jeffrey Lund <jefflund@gmail.com>
2017-09-14 10:09:21 -0600

    Added various dev repos to clone list

--- homedir/10-clone.py
+++ homedir/10-clone.py
@@ -9,12 +9,18 @@ REPOS = {
     'git@github.com:jefflund/gorl': 'go/src/github.com/jefflund/gorl',
     'git@github.com:jefflund/stones': 'go/src/github.com/jefflund/stones',
     'git@github.com:jefflund/modelt': 'go/src/github.com/jefflund/modelt',
+
     'git@github.com:jefflund/shelf': 'documents/shelf',
     'git@github.com:jefflund/website': 'documents/website',
     'git@github.com:jefflund/resume': 'documents/resume',
+
     'git@github.com:jefflund/ankura': 'research/ankura',
     'git@github.com:jefflund/writing': 'research/writing',
     'git@github.com:jefflund/reading': 'research/reading',
+
+    'git@github.com:jefflund/pssh': 'dev/pssh',
+    'git@github.com:jefflund/eoleof': 'dev/eoleof',
+    'git@github.com:jefflund/colorschemer': 'dev/colorschemer',
 }
 
 def main():
